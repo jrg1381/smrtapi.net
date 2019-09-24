9971fe3dbdc5d9fa733d3f86dacafb195f2218e4
James Gilmore <jamesg@speechmatics.com>
2019-09-24 17:33:51 +0100

    Add comment to demo app

--- SmRtAPI/DemoApp/Program.cs
+++ SmRtAPI/DemoApp/Program.cs
@@ -9,6 +9,14 @@ using Speechmatics.Realtime.Client.V2.Config;
 
 namespace DemoApp
 {
+    /*
+     This program is coded against an appliance using the v2 version of the API, releases 3.2.0 or above.
+    
+     To target a v1 appliance, change the V2 in `using Speechmatics.Realtime.Client.V2` and 
+     `using Speechmatics.Realtime.Client.V2.Config` to a V1.
+         
+     The v2 appliances have a compatibility layer which talks v1 protocol -- v2 is under wss://host:9000/v2, v1 is under wss://host:9000/.
+    */
     public class Program
     {
         private const string SampleAudio = "2013-8-british-soccer-football-commentary-alex-warner.mp3";
@@ -50,6 +58,7 @@ namespace DemoApp
                         OutputLocale = "en-GB",
                         AddTranscriptCallback = s => builder.Append(s),
                         AddTranscriptMessageCallback = s => Console.WriteLine(ToJson(s)),
+                        // The v2 appliances don't have partial transcripts, but rather "low-latency finals", so skip this bit.
                         //AddPartialTranscriptMessageCallback = s => Console.WriteLine(ToJson(s)),
                         ErrorMessageCallback = s => Console.WriteLine(ToJson(s)),
                         WarningMessageCallback = s => Console.WriteLine(ToJson(s)),
